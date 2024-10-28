d1e1afc3a2e864ba33f4a0c8c1a11eeafa80d7c2
Gabriela <1220832@isep.ipp.pt>
2024-10-28 11:25:32 +0000

    Commit - Ementas (Service e Repository)

---
+++ Grupo2A/Repositories/EmentasRepository.cs
@@ -0,0 +1,22 @@
+using Cozinha_BE.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Grupo2A.Repositories{
+    public class EmentasRepository{
+
+        private CozinhaContext _context;
+        public EmentasRepository(CozinhaContext context){
+            _context = context;
+        }
+
+         public async Task<IEnumerable<Prato>> GetEmentaDisponivel (DateTime data, int tipoDeRefeicaoId){
+            return await _context.Pratos
+            .Include(p=>p.TipoPrato)
+            .Include(p=>p.TipoRefeicao)
+            .Include(p=>p.Ingredientes)
+            .Where(p=>p.Data.Date == data.Date && p.TipoRefeicao.Id==tipoDeRefeicaoId)
+            .Where(p=>p.Quantidade>0 && p.Ativo == true)
+            .ToListAsync();
+        }
+    }
+}
---
+++ Grupo2A/Services/EmentasService.cs
@@ -0,0 +1,20 @@
+using Cozinha_BE.Model;
+using Cozinha_BE.Model.DTO;
+using Grupo2A.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Grupo2A.Services{
+    public class EmentasService{
+        private CozinhaContext _context;
+        private EmentasRepository _repo;
+        public EmentasService(CozinhaContext context){
+            _context = context;
+            _repo = new EmentasRepository(_context);
+        }
+
+        public async Task<IEnumerable<Prato>> GetEmentaDisponivel (DateTime data, int tipoDeRefeicaoId){
+            return await _repo.GetEmentaDisponivel(data, tipoDeRefeicaoId);
+        }
+    }
+}
