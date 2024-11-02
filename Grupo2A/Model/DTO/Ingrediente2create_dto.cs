55074b0f8d11452ff10db231e01073a177cd074b
Gabriela <1220832@isep.ipp.pt>
2024-11-02 01:52:36 +0000

    Commit - Coerência nos nomes

--- Grupo2A/Model/DTO/Ingrediente2create_dto.cs
+++ Grupo2A/Model/DTO/Ingrediente2create_dto.cs
@@ -2,7 +2,7 @@ namespace Cozinha_BE.Model.DTO;
 
 public class Ingrediente2create_dto
 {
-    public long Id { get; set; }
+    public int IdIngrediente { get; set; }
     public required string Name { get; set; }
 
     public required string Categoria { get; set; }
