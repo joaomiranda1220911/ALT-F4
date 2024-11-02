2ce2d47fabe0ce2d9489e4d772f43e9547f6444a
João Miranda <1220911@isep.ipp.pt>
2024-11-02 22:10:24 +0000

    migrations e database

---
+++ Grupo2A/Migrations/20241102220906_AddDescricaoTipoPratoColumn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Grupo2A.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescricaoTipoPratoColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
