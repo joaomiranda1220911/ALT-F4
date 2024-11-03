775839f5ea86a4bbd5c5851cc34f3ca3c511b305
AnaAlexandraMatos <1221035@isep.ipp.pt>
2024-11-03 14:28:48 +0000

    correção de erros - final!

--- Grupo2A/Model/DTO/Ingrediente2listing_dto.cs
+++ Grupo2A/Model/DTO/Ingrediente2listing_dto.cs
@@ -2,7 +2,7 @@ namespace Cozinha_BE.Model.DTO;
 
 public class Ingrediente2listing_dto
 {
-    public int IdIngrediente { get; set; }
+    public long IdIngrediente { get; set; }
     public required string Nome { get; set; }
     public required string Categoria { get; set; }
 }
