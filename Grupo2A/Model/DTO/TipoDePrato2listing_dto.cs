d5dd866795000844357f76dd142a0cb5909d6343
AnaAlexandraMatos <1221035@isep.ipp.pt>
2024-11-03 14:11:36 +0000

    corrigir erros

--- Grupo2A/Model/DTO/TipoDePrato2listing_dto.cs
+++ Grupo2A/Model/DTO/TipoDePrato2listing_dto.cs
@@ -1,7 +1,7 @@
 namespace Cozinha_BE.Model.DTO;
 
 public class TipoDePrato2listing_dto{
-    public int IdTipoPrato {get; set; }
+    public long IdTipoPrato {get; set; }
     public required string Nome {get; set;}
     public required string DescricaoTipoPrato {get; set;}
 }
--- Grupo2A/Model/TipoDePrato.cs
+++ Grupo2A/Model/TipoDePrato.cs
@@ -5,7 +5,7 @@ namespace Cozinha_BE.Model
     public class TipoDePrato
     {
         [Key]
-        public int IdTipoPrato { get; set; } // Identificador único do tipo de prato
+        public long IdTipoPrato { get; set; } // Identificador único do tipo de prato
 
         [StringLength(50)]
         public required string Nome { get; set; } // Nome do tipo de prato (ex: Carne, Peixe, Vegetariano)
