0a883a5e49c7c023cfa5fd3aa0e812b0b6a7e8fb
João Miranda <1220911@isep.ipp.pt>
2024-11-15 14:44:24 +0000

    app.js

--- Sala/sala/app.js
+++ Sala/sala/app.js
@@ -1,58 +1,27 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
+//Persistence ============
+var mongoose = require ('mongoose');
+mongoose.set('strictQuery', true);
+mongoose.connect ('mongodb+srv://TPSINF2user:TPSINF2pass@tpsinf2cluster.fbe14.mongodb.net/?retryWrites=true&w=majority&appName=TPSINF2Cluster')
+
+//Parser  =================================
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//Middleware ====================
+var mWare = require('./middleware');
+app.use(mWare);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//Routing  ====================
+var IssueRouter= require ('./routes/issueRoutes');
+app.use('/api/issues', IssueRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.locals.title='Error';
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-var express = require('express');
-var app = express();
-
-// Parser ==========================================
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-var port = 8080;
+var port=8080;
 app.listen(port);
-console.log('----------' + '\nUsing port' + port + '\n----------');
+console.log('--------------'+'\nUsing port'+ port + '\n--------------');
 
 module.exports = app;
