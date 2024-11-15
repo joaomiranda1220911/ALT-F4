f97dfe1dc3869c13b7e26a58efebf23bc33a3ae1
João Miranda <1220911@isep.ipp.pt>
2024-11-15 15:10:25 +0000

    middleware.js

---
+++ middleware.js
@@ -0,0 +1,8 @@
+// middleware to use for all requests
+var express = require('express');
+var router = express.Router();
+router.use(function(req, res, next) {
+console.log(req.method + ' : ' + req.url + ' | ' + JSON.stringify(req.body));
+next();
+});
+module.exports = router;
