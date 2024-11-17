0698c24311f8669ed3ab1e8a078236659376800a
Tiago Santos <1221040@isep.ipp.pt>
2024-11-17 14:12:07 +0000

    Routes - US009, US010

--- Sala/sala/routes/salaRoutes.js
+++ Sala/sala/routes/salaRoutes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const EmentaController = require('../controllers/ementaController');
 const EncomendaController = require('../controllers/encomendaController');
+const RefeicaoController = require('../controllers/refeicaoController');
 
 router.get('/ementa', EmentaController.getEmentaDisponivel);
 router.post('/encomendas', EncomendaController.encomendarPrato);
+//US009
+router.get('/refeicoes/:refeicaoId', RefeicaoController.listarRefeicoes);
+//US010
+router.get('/clientes/:clienteId/encomendas', EncomendaController.listarEncomendasCliente);
 
 module.exports = router;
