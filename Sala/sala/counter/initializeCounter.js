bc73d5c140075b36c1697a8e07d7d233d125814c
João Miranda <1220911@isep.ipp.pt>
2024-11-22 00:06:14 +0000

    counter

--- Sala/sala/counter/initializeCounter.js
+++ Sala/sala/counter/initializeCounter.js
@@ -1,8 +1,12 @@
 const Counter = require('../models/counter');
 
 async function initializeCounter() {
+    // Tenta encontrar um documento na coleção 'Counter' com _id igual a 'cliente_id'.
     const count = await Counter.findOne({ _id: 'cliente_id' });
 
+
+    // Se não encontrar o documento (o que significa que o contador ainda não foi inicializado),
+    // então cria um novo documento com o _id 'cliente_id' e a sequência inicializada com 0.
     if (!count) {
         await Counter.create({ _id: 'cliente_id', seq: 0 });  // Inicializa o contador com o valor 0
     }
--- Sala/sala/models/counter.js
+++ Sala/sala/models/counter.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 // Modelo para o contador de IDs
 const counterSchema = new mongoose.Schema({
+
+    // Define o campo '_id', que será do tipo String e é obrigatório (required).
     _id: { type: String, required: true },
+
+    // Define o campo 'seq', que será do tipo Number e tem um valor padrão de 0.
+    // Esse campo representa o valor do contador.
     seq: { type: Number, default: 0 }  // Valor inicial do contador
 });
 
