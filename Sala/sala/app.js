301e246e08f5a5baa415543f6b9420955508d1be
João Miranda <1220911@isep.ipp.pt>
2024-11-29 17:47:26 +0000

    update configuração cors

--- Sala/sala/app.js
+++ Sala/sala/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 // Configuração do CORS
 app.use(cors({
     origin: 'http://localhost:4200', // Permite apenas o frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Permite envio de cookies, se necessário
 }));
