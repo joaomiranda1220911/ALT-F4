c8ca1c4b38c43dc11c1dc715188a07eaf949e110
Seu Nome <1220827@isep.ipp.pt>
2024-11-29 01:50:08 +0000

    US006- ementaService.ts

--- Restaurante_FE/Restaurante/src/app/ementa-current/ementa-current.component.ts
+++ Restaurante_FE/Restaurante/src/app/ementa-current/ementa-current.component.ts
@@ -1,7 +1,32 @@
-// import {Component, OnInit } from '@angular/core';
-// import { HttpClient} from '@angular/common/http';
-// import{environment} from 'src/environment/environment';
+import { Component, OnInit } from '@angular/core';
+import { Ementa} from '@angular/common/http';
+import { NgIf } from @'angular/common';
+import { EmentaService} from '..Service/ementa.Service';
 
-// @Component({
+@Component({
+    selector: 'app-ementa-current',
+    standalone: true,
+    imports: [NgIf],
+    templateUrl: './ementa-current.component.html',
+    styleUrls: ['./ementa-current.component.cs']
+})
 
-// })
+export class ementaCurrentComponent implements OnInit {
+    ementa: Ementa | undefined;
+    errorMEssage: string | undefined;
+    loading: boolean = true;
+}
+
+ngOnInit(): void {
+    this.ementaSrv.getEmentaAtual().subscribe({
+      next: (resposta) => {
+        this.ementa = resposta; // Armazena a resposta do backend
+        this.loading = false; // Finaliza o carregamento
+      },
+      error: (err) => {
+        this.errorMessage = 'Erro ao carregar a ementa. Tente novamente mais tarde.';
+        this.loading = false; // Finaliza o carregamento em caso de erro
+      }
+    });
+  }
+}
