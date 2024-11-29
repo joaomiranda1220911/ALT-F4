c009160f9a11a8561a2e7168a16b6662afacd5bd
Seu Nome <1220827@isep.ipp.pt>
2024-11-29 19:23:49 +0000

    Ementa Atual- US006

--- Restaurante_FE/Restaurante/src/app/ementa-current/ementa-consultar.component.ts
+++ Restaurante_FE/Restaurante/src/app/ementa-current/ementa-consultar.component.ts
@@ -1,36 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { Ementa } from '@angular/common/http';
-import { NgIf } from @'angular/common';
-import { EmentaService } from '..Service/ementa.Service';
+import {tipoDeRefeicaoService} from '../Services/tipoDeRefeicao.service';
+import { EmentaService } from '../Services/ementa.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ementa-consultar',
   standalone: true,
-  imports: [NgIf],
   templateUrl: './ementa-consultar.component.html',
-  styleUrls: ['./ementa-consultar.component.cs']
+  styleUrls: ['./ementa-consultar.component.cs'],
 })
 
-export class ementaConsultarComponent implements OnInit {
-  ementa: any[] = []; //Lista pratos da emneta
+export class EmentaConsultarComponent implements OnInit {
+  ementaForm: FormGroup;
+  ementaDisponivel: any[] = []; //Lista pratos da emneta
   errorMessage: string = ''; //Mensagem de erro
+  successMessage: string = ''; 
+  tipoRefeicao: any[] = [];
 
-  constructor(private ementaSrv: EmentaService) { } // Injeção de dependência do serviço Ementa
-
+ constructor(
+  private fb: FormBuilder,
+  private ementaService: EmentaService,
+  private tipoDeRefeicaoService: tipoDeRefeicaoService,
+  private route: ActivatedRoute
+ ){}
 
   ngOnInit(): void {
-    this.consultarEmenta();
-  }
-
-  consultarEmenta(): void {
-    this.ementaSrv.getEmentaDisponivel().subscribe(
-      (response) => {
-        this.ementa = response;
-        this.errorMessage = '';
-      },
-      (error) => {
-        this.errorMessage = 'Erro ao obter a ementa. Tente novamente.';
-      }
-    );
+    this.ementaForm.this.fb.group({
+      data: ['', Validators.required],
+      tipoDeRefeicaoId: [null, Validators.required]
+    });
+      
   }
 }
