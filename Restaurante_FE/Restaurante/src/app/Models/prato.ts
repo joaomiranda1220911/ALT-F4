63cf5279861b35049f2e7c3da1a02bef1a0bba1e
Tiago Santos <1221040@isep.ipp.pt>
2024-12-02 20:41:19 +0000

    Organização de código - US 1 e 2

--- Restaurante_FE/Restaurante/src/app/Models/prato.ts
+++ Restaurante_FE/Restaurante/src/app/Models/prato.ts
@@ -1,8 +1,8 @@
 export interface Prato {
-    idPrato?: number; // ID único (opcional para criação)
+    idPrato?: number; // ID único 
     nome: string; // Nome do prato
     tipoPratoId: number; // ID do tipo de prato
     ingredientesIds: number[]; // IDs dos ingredientes necessários
-    receita?: string; // Receita do prato (opcional)
+    receita?: string; // Receita do prato 
     ativo: boolean; // Estado do prato (ativo ou inativo)
 }
